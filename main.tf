6fc94e4ef7d1a005e66fd148ce1556b3146fd2b7
Padma <padma.lingambhotla@gmail.com>
2024-08-23 14:18:22 -0700

    latest changes in main and Jenkins file

--- main.tf
+++ main.tf
@@ -2,17 +2,20 @@ provider "azurerm" {
   features {}
 }
 
+# Automatically retrieve the subscription ID
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_role_definition" "custom_role" {
   name        = "CustomRole"
   description = "Custom role with limited permissions"
-  scope       = "/subscriptions/${var.subscription_id}"
+  scope       = data.azurerm_client_config.current.subscription_id
 
   permissions {
     actions     = ["Microsoft.Storage/storageAccounts/read"]
     not_actions = []
   }
 
-  assignable_scopes = ["/subscriptions/${var.subscription_id}"]
+  assignable_scopes = [data.azurerm_client_config.current.subscription_id]
 }
 
 resource "azurerm_policy_definition" "custom_policy" {
@@ -36,15 +39,14 @@ resource "azurerm_policy_definition" "custom_policy" {
 
 resource "azurerm_subscription_policy_assignment" "custom_policy_assignment" {
   name                 = "customPolicyAssignment5"
-    policy_definition_id = azurerm_policy_definition.custom_policy.id
-  subscription_id                = "/subscriptions/${var.subscription_id}"
+  policy_definition_id = azurerm_policy_definition.custom_policy.id
+  subscription_id      = data.azurerm_client_config.current.subscription_id
 }
 
-
 resource "azurerm_role_assignment" "custom_role_assignment" {
-  principal_id   = var.principal_id
+  principal_id         = var.principal_id
   role_definition_name = azurerm_role_definition.custom_role.name
-  scope                = "/subscriptions/${var.subscription_id}"
+  scope                = data.azurerm_client_config.current.subscription_id
 
   depends_on = [azurerm_role_definition.custom_role]
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,5 @@
 # terraform.tfvars
 subscription_id = "4c326ad3-0ccf-4f2c-b529-17a931a9a3ba"
-principal_id    = "2834a465-1a9e-4ff5-ac74-665df8d6eee4"
+#principal_id    = "2834a465-1a9e-4ff5-ac74-665df8d6eee4"
+principal_id = "66ace9a8-e29f-4b1e-88b6-cafd3f00ce2c"
 
