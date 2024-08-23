2eda2d139b799aa8d627cb7c4cf83966b6e52a45
Padma <padma.lingambhotla@gmail.com>
2024-08-23 14:34:15 -0700

    latest changes in main

--- main.tf
+++ main.tf
@@ -1,24 +1,22 @@
 provider "azurerm" {
   features {}
-  subscription_id = data.azurerm_client_config.current.subscription_id
 }
 
-# Automatically retrieve the subscription ID
-data "azurerm_client_config" "current" {}
-
+# Custom Role Definition
 resource "azurerm_role_definition" "custom_role" {
   name        = "CustomRole"
   description = "Custom role with limited permissions"
-  scope       = data.azurerm_client_config.current.subscription_id
+  scope       = "/subscriptions/${var.subscription_id}"
 
   permissions {
     actions     = ["Microsoft.Storage/storageAccounts/read"]
     not_actions = []
   }
 
-  assignable_scopes = [data.azurerm_client_config.current.subscription_id]
+  assignable_scopes = ["/subscriptions/${var.subscription_id}"]
 }
 
+# Custom Policy Definition
 resource "azurerm_policy_definition" "custom_policy" {
   name         = "CustomPolicy5"
   policy_type  = "Custom"
@@ -38,16 +36,21 @@ resource "azurerm_policy_definition" "custom_policy" {
   POLICY
 }
 
+# Policy Assignment
 resource "azurerm_subscription_policy_assignment" "custom_policy_assignment" {
   name                 = "customPolicyAssignment5"
   policy_definition_id = azurerm_policy_definition.custom_policy.id
-  subscription_id      = data.azurerm_client_config.current.subscription_id
+  subscription_id      = var.subscription_id
 }
 
+# Role Assignment with Dependency
 resource "azurerm_role_assignment" "custom_role_assignment" {
   principal_id         = var.principal_id
   role_definition_name = azurerm_role_definition.custom_role.name
-  scope                = data.azurerm_client_config.current.subscription_id
+  scope                = "/subscriptions/${var.subscription_id}"
 
-  depends_on = [azurerm_role_definition.custom_role]
+  depends_on = [
+    azurerm_role_definition.custom_role,
+    azurerm_policy_definition.custom_policy
+  ]
 }
